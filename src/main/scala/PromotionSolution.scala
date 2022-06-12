d826a1042b171158ea7035d894d8ff6b7f2cb4d1
Anurag Komaravelli <anuragkomaravelli@Anurags-MacBook-Pro.local>
2022-06-12 02:21:21 -0700

    getting combinable promos for each promo, wip

--- src/main/scala/PromotionSolution.scala
+++ src/main/scala/PromotionSolution.scala
@@ -1,11 +1,44 @@
 import models.{Promotion, PromotionCombo}
 
-object PromotionSolution {
+object PromotionSolution extends App {
 
-    def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] = ???
+    val promos = Seq(
+        Promotion("P1", Seq("P3")),
+        Promotion("P2", Seq("P4,P5")),
+        Promotion("P3", Seq("P1")),
+        Promotion("P4", Seq("P2")),
+        Promotion("P5", Seq("P2")),
+    )
 
-    def combinablePromotions(
-                              promotionCode: String,
-                              allPromotions: Seq[Promotion]): Seq[PromotionCombo] = ???
+    private def getPromotionWithNonCombinableMap(promos: Seq[Promotion]): Map[String, Seq[String]] =
+        promos.groupBy(_.code).map {
+            case (k, v) => (k, v.flatMap(_.notCombinableWith))
+        }
 
+    def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] = {
+        val promosMap = getPromotionWithNonCombinableMap(allPromotions)
+        println(promosMap)
+        val allPromos: List[String] = promosMap.keySet.toList
+        for {
+            pr <- allPromos
+            notCombinable = promosMap(pr) :+ pr
+            _ = println("not combinable: " + notCombinable)
+            combinablePromos = allPromos.filter(!notCombinable.contains(_))
+            _ = println("combinable for promo " + pr + "\t" + combinablePromos)
+        } yield ()
+        //val asdf = recursive(allPromos)
+        //println(asdf)
+        Seq()
+    }
+
+
+    private def recursive(values: List[String], acc: List[String] = List.empty): Seq[String] = {
+        values match {
+            case Nil => acc
+            case value :: t => recursive(t, value :: acc)
+        }
+    }
+    allCombinablePromotions(promos)
+
+    def combinablePromotions(promotionCode: String, allPromotions: Seq[Promotion]): Seq[PromotionCombo] = ???
 }
