0f35c348000346f1c9916c332b8c5a10285e9e96
Anurag Komaravelli <anuragkomaravelli@Anurags-MacBook-Pro.local>
2022-06-14 08:56:10 -0700

    foldleft option for part 2

--- src/main/scala/PromotionSolution.scala
+++ src/main/scala/PromotionSolution.scala
@@ -1,7 +1,5 @@
 import models.{Promotion, PromotionCombo}
 
-import scala.annotation.tailrec
-
 object PromotionSolution extends App {
 
     val promos = Seq(
@@ -12,35 +10,85 @@ object PromotionSolution extends App {
         Promotion("P5", Seq("P2")),
     )
 
-    private def getPromotionWithNonCombinableMap(promos: Seq[Promotion]): Map[String, Seq[String]] =
+    private def promosNotAllowedMapping(promos: Seq[Promotion]): Map[String, Seq[String]] =
         promos.groupBy(_.code).map {
             case (k, v) => (k, v.flatMap(_.notCombinableWith))
         }
 
+    private def promosAllowedMapping(promos: Seq[Promotion], allPromos: Seq[String]): Map[String, Seq[String]] =
+        promos.groupBy(_.code).map {
+            case (k, v) => (k, allPromos.filter(p => !v.flatMap(_.notCombinableWith).contains(p) && p != k))
+        }
+
+
     def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] = {
-        val promosMap = getPromotionWithNonCombinableMap(allPromotions)
-        println(promosMap)
-        val allPromos: Seq[String] = promosMap.keySet.toSeq
-        val allCombinations = getAllPossibleCombinations(allPromos).filter(_.length > 1)
-        println(allCombinations)
+        val promosDenyMap = promosNotAllowedMapping(allPromotions)
+        val allPromos: Seq[String] = promosDenyMap.keySet.toSeq
+        val promosAllowMap = promosAllowedMapping(allPromotions, allPromos)
+        println(getCombinations(allPromos, promosAllowMap, promosDenyMap))
+        //println("promos allowed map: " + promosAllowMap)
 
-        val allCombinablePromotions = allCombinations.map {
-            combo => isEligibleCombination(combo, promosMap)
-        }
-        println("all combinable: " + allCombinablePromotions)
-
-        //val asdf = recursive(allPromos)
-        //println(asdf)
-        allCombinablePromotions.filter(_.length > 1).map {
-            cp => PromotionCombo(cp)
-        }
+        Seq()
     }
+
+
+
+    def getCombinations(
+        allPromos: Seq[String],
+        promosAllowMap: Map[String, Seq[String]],
+        promosDenyMap: Map[String, Seq[String]]
+    ) = {
+
+        val answerMaybe = allPromos.foldLeft((Seq[String](),Seq[String]()))((accumulator, p) => {
+            val allowList = if(!accumulator._1.contains(p) && !accumulator._2.contains(p)) {
+                accumulator._1 ++ Seq(p)
+            } else {
+                accumulator._1
+            }
+            val denyList = if(accumulator._2.contains(p)) accumulator._2 else accumulator._2 ++ promosDenyMap.getOrElse(p, Seq())
+
+            val result = allPromos.foldLeft((Seq[String](),Seq[String]()))((acc, promo) => {
+                println("accumulator: " + accumulator + "\t" + p)
+                //println("acc: " + acc + "\t" + promo)
+                if(acc._1.length + acc._2.length >= allPromos.length) {
+                    acc
+                }
+                val allowList = if(!acc._1.contains(p) && !acc._2.contains(p)) acc._1 ++ Seq(p) else acc._1
+                val denyList = if(acc._2.contains(p)) acc._2 else acc._2 ++ promosDenyMap.getOrElse(p, Seq())
+                (allowList, denyList)
+            })._1
+            //accumulator ++ Seq(result)
+        })
+        println("answer maybe? " + answerMaybe)
+
+        /*for {
+            promo1 <- allPromos
+            tmpList = Seq(promo1)
+            denyList = promosDenyMap.get(promo1) match {
+                case Some(actualList) => actualList
+                case _ => Seq()
+            }
+
+            _ = println(promo1 + "\t")
+        } yield ()*/
+    }
+
+    private def foldHelper(allPromos: Seq[String], promosDenyMap: Map[String, Seq[String]]) =
+        allPromos.foldLeft((Seq[String](),Seq[String]()))((acc, promo) => {
+            println("acc: " + acc + "\t" + promo)
+            if(acc._1.length + acc._2.length >= allPromos.length) {
+                acc
+            }
+            val allowList = if(!acc._1.contains(promo) && !acc._2.contains(promo)) acc._1 ++ Seq(promo) else acc._1
+            val denyList = if(acc._2.contains(promo)) acc._2 else acc._2 ++ promosDenyMap.getOrElse(promo, Seq())
+            (allowList, denyList)
+        })._1
 
     private def isEligibleCombination(combo: Seq[String], promosMap: Map[String, Seq[String]]) = {
        combo.flatMap {
            x => combo.flatMap {
-               y => if(!promosMap.get(x).contains(y)) {
-                   println("x|y: " + x + " " + y)
+               y => if(x != y && !promosMap.get(x).contains(y)) {
+                   //println("x|y: " + x + " " + y)
                    combo
                } else {
                    Seq()
